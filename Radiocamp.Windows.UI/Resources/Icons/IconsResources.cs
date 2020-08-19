d430bbe39c5628c7fe3e2482b3dc1957c3781b87
Dartware <dartware@mail.ru>
2020-08-20 01:31:30 +0300

    Add icons.

---
+++ Radiocamp.Windows.UI/Resources/Icons/IconsResources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace Dartware.Radiocamp.Windows.UI.Resources.Icons
+{
+	public static class IconsResources
+	{
+
+		public static String SortingIcon => GetIcon(nameof(SortingIcon));
+		public static String SortingDescendingIcon => GetIcon(nameof(SortingDescendingIcon));
+		public static String SortingAscendingIcon => GetIcon(nameof(SortingAscendingIcon));
+
+		public static String GetIcon(String resourceKey) => (String) Application.Current.FindResource(resourceKey);
+
+	}
+}
