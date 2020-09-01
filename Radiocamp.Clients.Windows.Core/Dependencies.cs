3f18f80f7b04c839e033630fbef246aed6a6d45a
Dartware <dartware@mail.ru>
2020-09-01 17:02:25 +0300

    Register settings service as dependency.

---
+++ Radiocamp.Clients.Windows.Core/Dependencies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dartware.Radiocamp.Clients.Windows.Core
+{
+	public static class Dependencies
+	{
+
+		private static IServiceProvider serviceProvider;
+
+		public static IServiceCollection Services { get; }
+
+		static Dependencies()
+		{
+			Services = new ServiceCollection();
+		}
+
+		public static void Build()
+		{
+			serviceProvider = Services.BuildServiceProvider();
+		}
+
+		public static TypeDefenition Get<TypeDefenition>() => serviceProvider.GetService<TypeDefenition>();
+
+	}
+}
--- Radiocamp.Clients.Windows/App.xaml.cs
+++ Radiocamp.Clients.Windows/App.xaml.cs
@@ -1,8 +1,28 @@
 ﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Dartware.Radiocamp.Clients.Windows.Core;
+using Dartware.Radiocamp.Clients.Windows.Database;
+using Dartware.Radiocamp.Clients.Windows.Services;
 
 namespace Dartware.Radiocamp.Clients.Windows
 {
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs args)
+		{
+			
+			base.OnStartup(args);
+
+			Dependencies.Services.AddDbContext<DatabaseContext>(builder =>
+			{
+				builder.UseSqlite("Data Source=Database.db");
+			});
+
+			Dependencies.Services.AddSingleton<ISettings, SettingsService>();
+
+			Dependencies.Build();
+
+		}
 	}
 }
