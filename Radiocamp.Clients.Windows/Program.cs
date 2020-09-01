650699714aeeb5b81bd8a70e1f90fe32af145463
Dartware <dartware@mail.ru>
2020-09-01 17:17:06 +0300

    Change entry point

---
+++ Radiocamp.Clients.Windows/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dartware.Radiocamp.Clients.Windows
+{
+	public sealed class Program
+	{
+		[STAThread]
+		public static void Main(String[] args)
+		{
+
+			if (args is null)
+			{
+				throw new ArgumentNullException(nameof(args));
+			}
+
+			App app = new App();
+
+			app.InitializeComponent();
+			app.Run();
+
+		}
+	}
+}
