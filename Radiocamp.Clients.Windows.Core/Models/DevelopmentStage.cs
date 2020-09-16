8298099551289808dfcfd8b6a8b8c662deaecd55
Dartware <dartware@mail.ru>
2020-09-17 00:06:14 +0300

    Add app logo resources

---
+++ Radiocamp.Clients.Windows.Core/Models/DevelopmentStage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dartware.Radiocamp.Clients.Windows.Core.Models
+{
+	public enum DevelopmentStage : Int32
+	{
+		Development = 0,
+		PreAlpha = 1,
+		Alpha = 2,
+		Beta = 3,
+		ReleaseCandidate = 4,
+		Release = 5
+	}
+}
